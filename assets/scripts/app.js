dadf4fff052827fd7cad6bc94ba1d53f12d83950
RobertBognar <robertbognar2@gmail.com>
2021-10-04 12:02:01 +0200

    demo mobile, tablet & desktop view

---
+++ assets/scripts/app.js
@@ -0,0 +1,4 @@
+//Importing variables & using param to define the rest of getElementById
+
+const searchBar = document.querySelector('.searchbar-container');
+const profileContainer = document.querySelector('.profile-container');
