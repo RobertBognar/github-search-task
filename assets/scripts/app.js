79578f2c6530f10e790468d5d4aa68407a2158e5
RobertBognar <robertbognar2@gmail.com>
2021-10-04 13:09:19 +0200

    importing html attributes to javascript

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,33 @@
-//Importing variables & using param to define the rest of getElementById
-
+//Importing variables & Root elements from CSS for toggle mode
+const root = document.documentElement.style;
 const searchBar = document.querySelector('.searchbar-container');
 const profileContainer = document.querySelector('.profile-container');
+//Using params for shorter code and passing it to getElementById
+const get = (param) => document.getElementById(`${param}`);
+//Header Params
+let btnMode = get('button-mode');
+let textMode = get('text-mode');
+let iconMode = get('mode-icon');
+//Searchbar Container Params
+let buttonSubmit = get('submit');
+let input = get('input');
+let noResult = get('no-results');
+//Profile Container Params
+let avatar = get('avatar');
+let userName = get('name');
+let user = get('user');
+let date = get('date');
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let bio = get('bio');
+//Profile Stats Container Params
+let repos = get('repos');
+let followers = get('followers');
+let following = get('following');
+//Profile Bottom Container Params
+let userLocation = get('location');
+let userPage = get('page');
+let userTwitter = get('twitter');
+let userCompany = get('company');
+let darkMode = false;
+//API URL
+const url = 'https://api.github.com/users/';
